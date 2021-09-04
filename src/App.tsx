4354648e696163a2c62f4ccd5acbf4a1692c031d
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-09-04 05:21:50 -0300

    fix: adding gesture handler root view for rectbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import 'react-native-gesture-handler'
-
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import Routes from './routes'
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <Routes />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Routes />
+    </GestureHandlerRootView>
   </NavigationContainer>
 )
 
